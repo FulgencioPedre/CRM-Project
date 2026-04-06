1e182055b12b42385bf38a8fa52281d7994c471e
Fulgencio Pedreño Robles <fulgenciopr@hotmail.com>
2026-04-06 14:27:34 +0200

    Added Contact Model

---
+++ MyCrm.Web/Models/Contact.cs
@@ -0,0 +1,17 @@
+﻿namespace MyCrm.Web.Models
+{
+    public class Contact
+    {
+        public Company company { get; set; };
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Surname { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+
+
+        public int CompanyId { get; set; }
+        public Company? Company { get; set; }
+    }
+}
