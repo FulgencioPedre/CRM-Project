6a8c60d58b6fb479fa932a0dcf21a9c9f229169f
Fulgencio Pedreño Robles <fulgenciopr@hotmail.com>
2026-04-06 14:16:47 +0200

    Added Models Folder and implemented Company Model

---
+++ MyCrm.Web/Models/Company.cs
@@ -0,0 +1,15 @@
+﻿namespace MyCrm.Web.Models
+{
+    public class Company
+    {
+        public List<Contact> Contacts { get; set; } = new List<Contact>();
+        public List<Proyect> Proyects { get; set; } = new List<Contact>();
+
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int CIF { get; set; }
+        public string Country { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+
+    }
+}
